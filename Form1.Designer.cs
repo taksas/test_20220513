e7f376f03f858d8c632a56734262fb2d6e75c8d7
tak_sas <takuminakayama4869@gmail.com>
2022-05-13 22:59:11 +0900

    Basic Scraping

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 28);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(182, 63);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(120, 87);
+            this.ClientSize = new System.Drawing.Size(206, 87);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
--- Form1.cs
+++ Form1.cs
@@ -24,32 +24,39 @@ namespace test_20220513
             for (int i = 1; i <= 60; i++)
             {
                 //Cookieを有効化?
-                var config = Configuration.Default.WithDefaultLoader().WithDefaultCookies(); // WithDefaultCookies()を追加
-                var context = BrowsingContext.New();
+                var config = Configuration.Default.WithDefaultLoader(); // WithDefaultCookies()を追加
+                var context = BrowsingContext.New(config);
                 //URLを取得
                 string hi = "https://tabelog.com/rstLst/sushi/" + i + "/";
                 var document = await context.OpenAsync(hi);
 
 
-                Debug.WriteLine(hi);
-                Debug.WriteLine(document.Title);
+                //Debug.WriteLine(hi);
+                //Debug.WriteLine(document.Title);
                 Debug.WriteLine("started");
 
                 try
                 {
-                    var classpList1 = document.GetElementsByClassName("list-rst__rst-name-target cpy-rst-name");
+                    //var classpList1 = document.GetElementsByClassName("list-rst__rst-name-target cpy-rst-name");
                     // var classpList1 = document.GetElementsByClassName("dimmed_text");
-                    //var classpList1 = document.QuerySelectorAll("href[class^='list-rst__rst-name-target cpy-rst-name']");
+                    var classpList1 = document.QuerySelectorAll("href[class^='list-rst__rst-name-target cpy-rst-name']");
                     //var classpList2 = document.QuerySelectorAll("div[href^='https://kadai-moodle.kagawa-u.ac.jp/course/view.php?id=']");
 
-                    for (var j = 0; j < classpList1.Length; j++)
-                        {
-                            var c = classpList1[j];
 
-                        Debug.WriteLine(c);
-
-                        
+                    foreach (var item in document.GetElementsByClassName("list-rst__rst-name-target cpy-rst-name"))
+                    {
+                        Debug.WriteLine(item.GetAttribute("href"));
                     }
+                    /*
+
+                                        for (var j = 0; j < classpList1.Length; j++)
+                                            {
+                                                var c = classpList1[j];
+
+                                            Debug.WriteLine(c);
+
+                                        }
+                    */
                 }
                 catch (System.Exception)
                 {
