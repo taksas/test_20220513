3a82066534c1208b9c036cf825a9971ed194a57d
tak_sas <takuminakayama4869@gmail.com>
2022-05-14 13:46:11 +0900

    fin

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Csv;
 using AngleSharp.Html.Parser;
 using AngleSharp.Html.Dom;
+using AngleSharp.Dom;
+using Jitbit.Utils;
+using System.Text.RegularExpressions;
+using System.Net;
 
 namespace test_20220513
 {
@@ -82,28 +85,131 @@ namespace test_20220513
             }
         }
 
-        // htmlパーサー
-        private static HtmlParser parser = new HtmlParser();
+
+
 
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            foreach (string line in System.IO.File.ReadLines(@"C:\Users\Public\Documents\outfile1.txt"))
+            var myExport = new CsvExport();
+            int num = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\Public\Documents\outfile2.txt"))  //店舗URL設定
             {
 
                 //Cookieを有効化?
                 var config = Configuration.Default.WithDefaultLoader(); // WithDefaultCookies()を追加
                 var context = BrowsingContext.New(config);
                 var document = await context.OpenAsync(line + "dtlmenu/");
-                IHtmlDocument ParserList = (IHtmlDocument)await parser.ParseDocumentAsync(document);
 
                 try
                 {
 
-                    foreach (var item in ParserList.GetElementsByClassName("rstdtl-menu-lst__contents"))
+                    foreach (var item in document.GetElementsByClassName("rstdtl-menu-lst__contents"))
                     {
+                        foreach (var item2 in item.GetElementsByClassName("js-imagebox-trigger"))
+                        {
+                            if (item2.GetAttribute("href") != null)
+                            {
+                                foreach (var itemname in item.GetElementsByClassName("rstdtl-menu-lst__menu-title")) {  
+                                    foreach (var itemprice in item.GetElementsByClassName("rstdtl-menu-lst__price"))
+                                    {
+                                        foreach (string line2 in System.IO.File.ReadLines(@"C:\Users\Public\Documents\neta2.txt"))
+                                        {
+                                            if (itemname.TextContent.Contains(line2.Remove(0, 2)) 
+                                                && !itemname.TextContent.Contains("刺")
+                                                && !itemname.TextContent.Contains("さしみ")
+                                                && !itemname.TextContent.Contains("手作り")
+                                                && !itemname.TextContent.Contains("サラダ")
+                                                && !itemname.TextContent.Contains("焼")
+                                                && !itemname.TextContent.Contains("丼")
+                                                && !itemname.TextContent.Contains("比べ")
+                                                && !itemname.TextContent.Contains("飯")
+                                                && !itemname.TextContent.Contains("まき")
+                                                && !itemname.TextContent.Contains("巻")
+                                                && !itemname.TextContent.Contains("切")
+                                                && !itemname.TextContent.Contains("g")
+                                                && !itemname.TextContent.Contains("本")
+                                                && !itemname.TextContent.Contains("個")
+                                                && !itemname.TextContent.Contains("鍋")
+                                                && !itemname.TextContent.Contains("汁")
+                                                && !itemname.TextContent.Contains("鉢")
+                                                && !itemname.TextContent.Contains("蒸")
+                                                && !itemname.TextContent.Contains("膳")
+                                                && !itemname.TextContent.Contains("塩辛")
+                                                && !itemname.TextContent.Contains("たこわさ")
+                                                && !itemname.TextContent.Contains("活")
+                                                && !itemname.TextContent.Contains("雑炊")
+                                                && !itemname.TextContent.Contains("ザンギ")
+                                                && !itemname.TextContent.Contains(" ハラス")
+                                                && !itemname.TextContent.Contains("のせ")
+                                                && !itemname.TextContent.Contains("げそ")
+                                                && !itemname.TextContent.Contains("ゲソ")
 
-                            Debug.WriteLine($"{item.GetAttribute("href")}こん{item.GetAttribute("href")}");
+                                                && itemname.TextContent
+
+                                                .Replace(line2.Remove(0, 2), "")
+                                                .Replace("寿司", "")
+                                                .Replace("握り", "")
+                                                .Replace("道産", "")
+                                                .Replace("県産", "")
+                                                .Replace("国産", "")
+                                                .Replace("産", "")
+                                                .Replace("まぐろ", "")
+                                                .Replace("マグロ", "")
+                                                .Replace("鮪", "")
+                                                .Replace("生", "")
+                                                .Replace("本", "")
+                                                .Replace("本場", "")
+                                                .Replace("名物", "")
+                                                .Replace("赤身", "")
+                                                .Replace("身", "")
+                                                .Replace("トロ", "")
+                                                .Replace("とろ", "")
+                                                .Replace("名物", "")
+                                                .Replace("特上", "")
+                                                .Replace("上", "")
+                                                
+
+
+
+                                                .Length <= 3
+
+                                                )
+                                            {
+
+                                                string itempricestr = Regex.Replace(itemprice.TextContent, @"[^0-9]", "");
+
+                                                WebClient wc = new WebClient();
+                                                try
+                                                {
+                                                    wc.DownloadFile(item2.GetAttribute("href"), "C:\\Users\\Public\\Documents\\sushi\\" + num++ + ".jpg");
+
+                                                }
+                                                catch (WebException exc)
+                                                {
+                                                    throw;
+                                                }
+
+
+                                                Debug.WriteLine($"{item2.GetAttribute("href")}／{num}／{itemname.TextContent}／{line2.Substring(0, 2).Replace("/", "")}／{itempricestr}");
+
+                                                myExport.AddRow();
+                                                myExport["imageurl"] = num;
+                                                myExport["name"] = itemname.TextContent;
+                                                myExport["num"] = line2.Substring(0, 2).Replace("/", "");
+                                                myExport["price"] = itempricestr;
+
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            
+
+
+                        }
 
 
                     }
@@ -113,7 +219,10 @@ namespace test_20220513
                 {
                     throw;
                 }
+                File.WriteAllBytes("C:\\Users\\Public\\Documents\\traindata1.csv", myExport.ExportToBytes());
             }
+            
         }
+
     }
 }
